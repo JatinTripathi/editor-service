85d97a5bd1b9b45bc484bc19116f72384a55afc1
Jatin Tripathi <pratap.jatintripathi@gmail.com>
2016-03-12 22:06:03 +0530

    Update editorAjax.js

--- public/js/editorAjax.js
+++ public/js/editorAjax.js
@@ -1,10 +1,18 @@
-var paperContent=$("#editorContent").serializeArray();
+var doc={};
+doc.title=$("#title").html();
+doc.shortScript=$("#shortScript").html();
+doc.body=$("#body").html();
+
+var paper=JSON.stringify(doc);
+
+var surl="http://localhost:8080/editor/save";
+var purl="http://localhost:8080/editor/publish";
 
 $(document).ready(function(){
     $('#save').click(function(){
-        $.post('/editor/save',paperContent,function(data,textStatus){alert('');});
+        $.post(surl,paper,function(data,textStatus){alert('');});
     });
     $('#publish').click(function(){
-        $.post('/editor/publish',paperContent,function(data,textStatus){alert('');});
+        $.post(purl,paper,function(data,textStatus){alert('');});
     });
 });
